c40f3beb9ba33dac986cf281c196c28863cb6808
Prevalence <945926945@qq.com>
2016-05-08 18:36:57 +0800

    初始版本（带初步注释）

---
+++ BF/src/runner/ClientRunner.java
@@ -0,0 +1,52 @@
+package runner;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import rmi.RemoteHelper;
+import service.IOService;
+import ui.MainFrame;
+
+public class ClientRunner {
+	private RemoteHelper remoteHelper;
+	
+	public ClientRunner() {//构造方法，建立了一个界面
+		linkToServer();
+		initGUI();
+	}
+	
+	private void linkToServer() {
+		try {
+			remoteHelper = RemoteHelper.getInstance();
+			//把远程对象注册到RMI注册服务器上，并命名为DataRemoteObject?
+			remoteHelper.setRemote(Naming.lookup("rmi://localhost:8888/DataRemoteObject"));
+			System.out.println("linked");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void initGUI() {
+		MainFrame mainFrame = new MainFrame();
+	}
+	
+	public void test(){
+		try {
+			System.out.println(remoteHelper.getUserService().login("admin", "123456a"));
+			System.out.println(remoteHelper.getIOService().writeFile("2", "admin", "testFile"));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args){//主程序
+		ClientRunner cr = new ClientRunner();
+		//cr.test();
+	}
+}
